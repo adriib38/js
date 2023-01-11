92e1b56ad72ecb97244662a066bd2b63d8d6eb13
adrian <adrianbenitezb7@gmail.com>
2023-01-11 19:26:37 +0100

    bucles

---
+++ bucles.js
@@ -0,0 +1,29 @@
+
+let personas = ["Adrian", "Juan", "Paco"];
+
+let edad = 20;
+
+/** Condicionales */
+
+if(edad >= 18){
+    console.log('Mayor de edad');
+}else{
+    console.log('Menor de edad');
+}
+
+/** Bucles */
+//FOR
+for (let i = 0; i < personas.length; i++) {
+    console.log(personas[i])
+}
+
+//WHILE
+let indice = 0;
+while (indice < personas.length) {
+    console.log(personas[indice]);
+    indice++;
+}
+
+do{
+
+}while(numero <=)
